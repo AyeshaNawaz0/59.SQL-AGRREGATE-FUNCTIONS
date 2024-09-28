b7e49a50342bf706b6deac220258862f93ff0121
AyeshaNawaz0 <zawanahseya@gmail.com>
2024-09-28 19:19:47 +0500

    a

--- AggregateFunctions.sql
+++ AggregateFunctions.sql
@@ -1,48 +1,30 @@
-CREATE TABLE IF NOT EXISTS DEPARTMENT (
-  EMPLOYEE_ID TEXT,
-  NAME TEXT,
-  DEPARTMENT_ID TEXT,
-  MANAGER_ID TEXT,
-  SALARY REAL
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    name TEXT,
+    dep_id INT,
+    salary INT
 );
+INSERT INTO employee(id,name,dep_id,salary)VALUES
+(1, "Ali", 01, 20000),
+(2, "Zara", 02, 40000),
+(3, "Hamza", 01, 30000),
+(4, "Hamna", 02, 50000);
 
-INSERT INTO DEPARTMENT (EMPLOYEE_ID, NAME, DEPARTMENT_ID, MANAGER_ID, SALARY) VALUES
-  ('100', 'STEVEN KING', '90', '100', 24000),
-  ('101', 'NEENA KOCCHAR', '90', '100', 17000),
-  ('102', 'LEX DEHAAN', '90', '102', 9000),
-  ('103', 'BRUCE LEE', '60', '103', 4800),
-  ('104', 'DIANA WILLS', '60', '103', 25000),
-  ('105', 'VALLI PATOR', '50', '100', 4200),
-  ('1973', 'LUV HAMI', '60', '102', 5000),
-  ('106', 'DAVID AUSTIN', '90', '100', 6000);
+SELECT * FROM employee;
 
--- Query to count the number of employees in each department
-SELECT department_id AS "Department Code", 
-COUNT(*) AS "No of Employees" 
-FROM DEPARTMENT
-GROUP BY department_id; 
+SELECT AVG(salary) as avg_price
+FROM employee;
 
--- Query to sum the salary of each department
-SELECT department_id, SUM(salary) 
-FROM DEPARTMENT 
-GROUP BY department_id;
+SELECT SUM(salary) as sum_price
+FROM employee;
 
--- Query to count the number of employees and sum the salary in each department
-SELECT department_id AS "Department Code", 
-COUNT(*) AS "No of Employees", 
-SUM(salary) AS "Total Salary" 
-FROM DEPARTMENT 
-GROUP BY department_id; 
+SELECT COUNT(dep_id) as dep_count
+FROM employee;
 
--- Query to sum the salary of employees with a specific manager
-SELECT department_id AS "Department Code", 
-SUM(salary) AS "Total Salary" 
-FROM DEPARTMENT 
-WHERE MANAGER_ID = '103' 
-GROUP BY department_id;
+SELECT salary
+FROM employee
+WHERE salary = (SELECT MIN(salary) as min_salary FROM employee);
 
--- Query to find departments with more than 2 employees
-SELECT department_id, COUNT(*) AS "No. of Employees" 
-FROM DEPARTMENT 
-GROUP BY department_id 
-HAVING COUNT(*) > 2;
+SELECT salary
+FROM employee
+WHERE salary = (SELECT MAX(salary) as max_salary FROM employee);    
